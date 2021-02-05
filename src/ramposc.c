265db632267a1c50c67101a01f575979d5da0edf
U-MSI\Ben <bensergentanis@gmail.com>
2021-02-05 12:02:06 -0500

    create ramposc module

---
+++ src/ramposc.c
@@ -0,0 +1,13 @@
+#include "ramposc.h"
+
+float RampOscInit(float sr, RampOsc* osc){
+	osc->phase_ = 0.f;
+	osc->phase_inc_ = 2.f * 440.f / sr; //-1 to 1
+	osc->sr_ = sr;
+}
+
+float RampOscProcess(RampOsc* osc){
+	osc->phase_ += osc->phase_inc_;
+	if(osc->phase_ >= 1.f) osc->phase_ -= 2.f;
+	return osc->phase_;
+}
---
+++ src/ramposc.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef MRG_RAMPOSC_H
+#define MRG_RAMPOSC_H
+
+#include <stdint.h>
+
+typedef struct{
+	float phase_;     //current phase -1.f to 1.f
+	float phase_inc_; //how much to inc every time process is called
+	float sr_;        //engine samplerate
+} RampOsc;
+
+#endif
